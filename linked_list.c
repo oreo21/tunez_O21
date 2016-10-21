772a1524e3ad5d212c30163baaf985c667be9cc2
oreo21 <reok99@gmail.com>
2016-10-21 00:24:38 -0400

    makefile done, rename files

--- linked_list.c
+++ linked_list.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+typedef struct song_node{
+  name char[256];
+  artist char[256];
+  struct song_node *next;
+} song_node;
+
 void print_list(struct song_node *start){
   printf("%s - %s,  ", start -> artist, start -> name);
   if (start -> next != 0) print_list(start -> next);
@@ -16,8 +22,13 @@ song_node* insert_front(song_node *list, char *n, char *a){
 }
 
 song_node* insert_order(song_node *list, char *n, char *a){
-  song_node *newNode = (song_node *)malloc(sizeof(song_node));
-  int foundLoc = 0;
+  song_node *curr = list;
+  int foundPos = 0; //0 = start, 1 = artist found, 2 = song found
+  while (foundPos){
+    if (strcmp(n, curr -> artist) < 0)
+    	foundPos = 1;
+
+  }
 }
 
 song_node* find_song(song_node *list, char *s){
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-compile: myTunez.c
-	gcc myTunez.c -o myTunez
+compile: main.c linked_list.c linked_list.h library.c library.h
+	gcc main.c linked_list.c library.c -o app
 
 clean: 
 	rm *~
 
-run: myTunez
-	./myTunez
+run: compile
+	./app
